438ad3cf132f2ea9f51282a5a73f7d74703457b3
whitgunt77 <whitgunt77@gmail.com>
2025-11-24 11:47:23 -0500

    Create auth.js

---
+++ server/middleware/auth.js
@@ -0,0 +1,14 @@
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+
+module.exports = async function(req, res, next) {
+  const token = req.header('Authorization')?.replace('Bearer ', '');
+  if (!token) return res.status(401).json({ msg: 'No token' });
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = await User.findById(decoded.id).select('-password');
+    next();
+  } catch (err) {
+    res.status(401).json({ msg: 'Token invalid' });
+  }
+}
