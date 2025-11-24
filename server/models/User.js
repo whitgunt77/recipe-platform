60a153e714855a5eab89f3c9c6efe85fb8df92d0
whitgunt77 <whitgunt77@gmail.com>
2025-11-24 11:40:26 -0500

    Create User.js

---
+++ server/models/User.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const UserSchema = new mongoose.Schema({
+  name: { type: String },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  favorites: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Recipe' }]
+}, { timestamps: true });
+
+UserSchema.pre('save', async function(next){
+  if (!this.isModified('password')) return next();
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+UserSchema.methods.comparePassword = function(candidate){
+  const bcrypt = require('bcryptjs');
+  return bcrypt.compare(candidate, this.password);
+};
+
+module.exports = mongoose.model('User', UserSchema);
